2607247485bca726b1ec6909549f9cd8426bacf9
umidnm <umidnm@code.edu.az>
2023-04-18 17:34:36 +0400

    first try MVC

---
+++ BackIntro/BackIntro/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+      public ViewResult Index()
+        {
+            ViewResult result = View();
+            result.ViewName = "Index";
+            return result;
+
+        }
+        public ViewResult About()
+        {
+            ViewResult result = View();
+            result.ViewName = "About";
+            return result;
+
+        }
+
+        public ViewResult Contact()
+        {
+            ViewResult result = View();
+            result.ViewName = "Contact";
+            return result;
+
+        }
+
+
+
+    }
+}
---
+++ BackIntro/BackIntro/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackIntro.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ContentResult Detail(int id)
+        {
+           
+
+            return Content(id.ToString());
+        }
+        public ViewResult Index()
+        {
+            return View();
+
+        }
+       
+       
+        
+    }
+}
